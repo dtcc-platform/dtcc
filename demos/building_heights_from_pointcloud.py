770f25953761f3faa69dee18654bec7932ea1a70
Dag Wästberg <dwastberg@gmail.com>
2024-02-21 17:53:42 +0100

    fix demo

--- demos/building_heights_from_pointcloud.py
+++ demos/building_heights_from_pointcloud.py
@@ -14,16 +14,20 @@ footprints = dtcc.io.load_footprints(buildings_path, "uuid")
 pc = dtcc.io.load_pointcloud(pointcloud_path)
 pc = pc.remove_global_outliers(3)
 
-terrain = dtcc.builder.build_terrain_raster(pc, cell_size=2, radius=3, ground_only=True)
+terrain_raster = dtcc.builder.build_terrain_raster(
+    pc, cell_size=2, radius=3, ground_only=True
+)
+terrain_mesh = dtcc.builder.build_terrain_mesh(terrain_raster)
 
 footprints = dtcc.builder.extract_roof_points(footprints, pc)
-footprints = dtcc.builder.compute_building_heights(footprints, terrain)
+footprints = dtcc.builder.compute_building_heights(
+    footprints, terrain_raster, overwrite=True
+)
 
 lod1_buildings = dtcc.builder.build_lod1_buildings(footprints)
-print(lod1_buildings[157].lod0.vertices)
 city = dtcc.City()
-city.add_terrain(terrain)
+city.add_terrain(terrain_raster)
+city.add_terrain(terrain_mesh)
 city.add_buildings(lod1_buildings)
 
-print(city.buildings[157].geometry[dtcc.GeometryType.LOD1])
-# city.view()
+city.view()

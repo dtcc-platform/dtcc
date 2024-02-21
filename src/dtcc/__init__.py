591015606011f00b064f2c5db0abeeaa6f7e9920
Dag Wästberg <dwastberg@gmail.com>
2024-02-21 14:54:06 +0100

    new demo for new data model

--- src/dtcc/__init__.py
+++ src/dtcc/__init__.py
@@ -5,23 +5,25 @@ import dtcc_io as io
 import dtcc_data as data
 import dtcc_wrangler as wrangler
 import dtcc_builder as builder
-import dtcc_viewer as viewer
+
+# import dtcc_viewer as viewer
 
 
 # Collect __all__ from submodules
-modules = [common, model, io, data, wrangler, builder, viewer]
+modules = [common, model, io, data, wrangler, builder]  # , viewer]
 __all__ = []
 for module in modules:
     for name in module.__all__:
         globals()[name] = getattr(module, name)
     __all__ += module.__all__
 
-    
+
 # Local imports
 from .logging import debug, info, warning, error, critical
+
 __all__ += ["debug", "info", "warning", "error", "critical"]
 
-    
+
 # Import parameters from dtcc-builder. We should think about how to do this in a
 # good way, perhaps we can have a common parameter set defined in dtcc-common
 # and then all modules extend the parameter set with their own parameters.

9de64ae27ace3d0039103ed20fce1226ab3b3368
Dag Wästberg <dwastberg@gmail.com>
2024-05-08 14:47:44 +0200

    building footprint demo

--- demos/simplify_building_footprints.py
+++ demos/simplify_building_footprints.py
@@ -14,7 +14,6 @@ pc = pc.remove_global_outliers(3)
 terrain_raster = dtcc.builder.build_terrain_raster(
     pc, cell_size=2, radius=3, ground_only=True
 )
-terrain_mesh = dtcc.builder.build_terrain_mesh(terrain_raster)
 
 footprints = dtcc.builder.extract_roof_points(
     footprints, pc, statistical_outlier_remover=True
@@ -23,16 +22,29 @@ footprints = dtcc.builder.compute_building_heights(
     footprints, terrain_raster, overwrite=True
 )
 
-merged_footprints = dtcc.builder.merge_building_footprints(footprints, lod=dtcc.model.GeometryType.LOD0, max_distance=0.5, min_area=10)
-simplifed_footprints = dtcc.builder.simplify_building_footprints(merged_footprints, 0.25, lod=dtcc.model.GeometryType.LOD0)
+merged_footprints = dtcc.builder.merge_building_footprints(
+    footprints, lod=dtcc.model.GeometryType.LOD0, max_distance=0.5, min_area=10
+)
+simplifed_footprints = dtcc.builder.simplify_building_footprints(
+    merged_footprints, 0.25, lod=dtcc.model.GeometryType.LOD0
+)
 clearance_fix = dtcc.builder.fix_building_footprint_clearance(simplifed_footprints, 0.5)
+
+# height = clearance_fix[0].height
+
+max_wall_length = [building.height / 3 for building in clearance_fix]
+
+
+split_walls = dtcc.builder.split_footprint_walls(
+    clearance_fix, max_wall_length=max_wall_length
+)
 
 # merged_footprints = merged_footprints[63:66]
 
 
 city = dtcc.City()
-# city.add_terrain(terrain_mesh)
-city.add_buildings(clearance_fix)
-
-
+# # city.add_terrain(terrain_mesh)
+city.add_buildings(split_walls)
+#
+#
 city.view()

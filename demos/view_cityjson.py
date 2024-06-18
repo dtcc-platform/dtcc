00113cfdd43752cb841d10f8b447262efafea3c4
Anders Logg <logg@chalmers.se>
2024-06-18 11:31:43 +0200

    Demo polishing

--- demos/view_cityjson.py
+++ demos/view_cityjson.py
@@ -2,11 +2,8 @@
 
 import dtcc
 
-# Set data directory (and check that it exists)
-data_directory = dtcc.set_data_directory("data/cityjson")
-
 # Load city model from CityJSON file
-city = dtcc.load_city(data_directory / "DenHaag_01.city.json")
+city = dtcc.load_city("data/cityjson/DenHaag_01.city.json")
 
 # View city
 city.view()
--- demos/view_pointcloud.py
+++ demos/view_pointcloud.py
@@ -2,11 +2,8 @@
 
 import dtcc
 
-# Set data directory (and check that it exists)
-data_directory = dtcc.set_data_directory("data/helsingborg-residential-2022")
-
 # Load point cloud from LAS file
-pc = dtcc.load_pointcloud(data_directory / "pointcloud.las")
+pc = dtcc.load_pointcloud("data/helsingborg-residential-2022/pointcloud.las")
 
 # View point cloud (color by z-coordinate)
 color_data = pc.points[:, 2]

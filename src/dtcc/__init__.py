7ad9b49142d785f14e8fada27bd5d91e2a34d414
georspai <spaikgeo@gmail.com>
2024-09-30 13:53:34 +0300

    Package consolidation and import changes

--- src/dtcc/__init__.py
+++ src/dtcc/__init__.py
@@ -1,15 +1,16 @@
 # Import submodules
-import dtcc_common as common
-import dtcc_model as model
-import dtcc_io as io
-import dtcc_data as data
-import dtcc_atlas as atlas
-import dtcc_builder as builder
+from dtcc_core import common as common
+from dtcc_core import model as model
+from dtcc_core import io as io
+from dtcc_core import builder as builder
+
+# import dtcc_data as data
+# import dtcc_atlas as atlas
 import dtcc_viewer as viewer
 
 
 # Collect __all__ from submodules
-modules = [common, model, io, data, builder, viewer, atlas]
+modules = [common, model, io, builder, viewer,]
 __all__ = []
 for module in modules:
     for name in module.__all__:
@@ -26,6 +27,8 @@ __all__ += ["debug", "info", "warning", "error", "critical"]
 # Import parameters from dtcc-builder. We should think about how to do this in a
 # good way, perhaps we can have a common parameter set defined in dtcc-common
 # and then all modules extend the parameter set with their own parameters.
-from dtcc_builder import parameters
+from dtcc_core.builder import parameters
 
+# __all__.append("atlas")
+# __all__.append("data")
 __all__.append("parameters")

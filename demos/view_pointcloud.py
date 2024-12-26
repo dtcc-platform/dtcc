c3744a274273a78a89657e1e52d5788db5cb6531
Vasilis Naserentin <125367195+vbassn@users.noreply.github.com>
2024-12-26 12:58:02 +0200

    Same for other demo

--- demos/view_pointcloud.py
+++ demos/view_pointcloud.py
@@ -1,9 +1,12 @@
 # This demo illustrates how to load and view a point cloud.
 
 import dtcc
+from pathlib import Path
 
+data_directory = Path(__file__).parent / ".." / "data" / "helsingborg-residential-2022"
+pointcloud_path = data_directory / "pointcloud.las"
 # Load point cloud from LAS file
-pc = dtcc.load_pointcloud("data/helsingborg-residential-2022/pointcloud.las")
+pc = dtcc.load_pointcloud(pointcloud_path)
 
 # View point cloud (color by z-coordinate)
 color_data = pc.points[:, 2]

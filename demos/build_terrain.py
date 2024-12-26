f2cbcf49c1df7beb9e4093a459264a091ecc076b
Vasilis Naserentin <125367195+vbassn@users.noreply.github.com>
2024-12-26 12:52:14 +0200

    Change in build_terrain.py to fix Windows demos CI

--- demos/build_terrain.py
+++ demos/build_terrain.py
@@ -1,9 +1,13 @@
 # This demo illustrates how to build a terrain from a point cloud.
 
 import dtcc
+from pathlib import Path
 
 # Load point cloud from LAS file
-pointcloud = dtcc.load_pointcloud("data/helsingborg-residential-2022/pointcloud.las")
+
+data_directory = Path(__file__).parent / ".." / "data" / "helsingborg-residential-2022"
+pointcloud_path = data_directory / "pointcloud.las"
+pointcloud = dtcc.load_pointcloud(pointcloud_path)
 
 # Remove global outliers
 pointcloud = pointcloud.remove_global_outliers(3.0)

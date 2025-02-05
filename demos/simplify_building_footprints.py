f3d4b13ed3ef18422f57d144e4e83467b3f86ff1
georspai <spaikgeo@gmail.com>
2025-02-05 14:15:03 +0200

    Demos use dtcc_data to fetch footprints/lidar

--- demos/simplify_building_footprints.py
+++ demos/simplify_building_footprints.py
@@ -1,42 +1,43 @@
 import dtcc
-from pathlib import Path
 
-
-data_directory = (
-    Path(__file__).parent.resolve() / "../data/helsingborg-residential-2022"
+# Define the bounding box for the Helsingborg residential area. 
+# These coordinates specify the area of interest to be passed into the 
+bounds = dtcc.Bounds(
+    xmin=319891,
+    ymin=6399790,
+    xmax=319891+2000,
+    ymax=6399790+2000
 )
-buildings_path = data_directory / "footprints.shp"
-pointcloud_path = data_directory / "pointcloud.las"
 
-footprints = dtcc.io.load_footprints(buildings_path, "uuid")
+footprints = dtcc.download_data(data_type='footprints', provider='dtcc', user_bbox=bounds)
 
-pc = dtcc.io.load_pointcloud(pointcloud_path)
+pc = dtcc.download_data(data_type='lidar', provider='dtcc', user_bbox=bounds)
 pc = pc.remove_global_outliers(3)
 
-terrain_raster = dtcc.builder.build_terrain_raster(
+terrain_raster = dtcc.build_terrain_raster(
     pc, cell_size=2, radius=3, ground_only=True
 )
 
-footprints = dtcc.builder.extract_roof_points(
+footprints = dtcc.extract_roof_points(
     footprints, pc, statistical_outlier_remover=True
 )
-footprints = dtcc.builder.compute_building_heights(
+footprints = dtcc.compute_building_heights(
     footprints, terrain_raster, overwrite=True
 )
 
-merged_footprints = dtcc.builder.merge_building_footprints(
-    footprints, lod=dtcc.model.GeometryType.LOD0, max_distance=0.5, min_area=10
+merged_footprints = dtcc.merge_building_footprints(
+    footprints, lod=dtcc.GeometryType.LOD0, max_distance=0.5, min_area=10
 )
-simplifed_footprints = dtcc.builder.simplify_building_footprints(
-    merged_footprints, 0.25, lod=dtcc.model.GeometryType.LOD0
+simplifed_footprints = dtcc.simplify_building_footprints(
+    merged_footprints, 0.25, lod=dtcc.GeometryType.LOD0
 )
-clearance_fix = dtcc.builder.fix_building_footprint_clearance(simplifed_footprints, 0.5)
+clearance_fix = dtcc.fix_building_footprint_clearance(simplifed_footprints, 0.5)
 
 
 max_wall_length = [building.height for building in clearance_fix]
 
 
-split_walls = dtcc.builder.split_footprint_walls(
+split_walls = dtcc.split_footprint_walls(
     clearance_fix, max_wall_length=max_wall_length
 )
 
--- demos/view_pointcloud.py
+++ demos/view_pointcloud.py
@@ -1,12 +1,17 @@
 # This demo illustrates how to load and view a point cloud.
 
 import dtcc
-from pathlib import Path
 
-data_directory = Path(__file__).parent / ".." / "data" / "helsingborg-residential-2022"
-pointcloud_path = data_directory / "pointcloud.las"
-# Load point cloud from LAS file
-pc = dtcc.load_pointcloud(pointcloud_path)
+# Define the bounding box for the Helsingborg residential area. 
+# These coordinates specify the area of interest to be passed into the 
+bounds = dtcc.Bounds(
+    xmin=319891,
+    ymin=6399790,
+    xmax=319891+2000,
+    ymax=6399790+2000
+)
+
+pc = dtcc.download_data(data_type='lidar', provider= 'dtcc', user_bbox=bounds)
 
 # View point cloud (color by z-coordinate)
 color_data = pc.points[:, 2]

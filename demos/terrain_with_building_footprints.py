e3b1d9abe870972045cce209a5045295f379a5f1
Anders Logg <logg@chalmers.se>
2025-02-06 14:49:50 +0100

    Demo polishing

--- demos/terrain_with_building_footprints.py
+++ demos/terrain_with_building_footprints.py
@@ -1,48 +1,42 @@
-# Copyright (C) 2023 Dag Wästberg
-# Licensed under the MIT License
-#
-# This demo illustrates how to generate a terrain mesh from a point cloud.
+# This demo illustrates how to build a terrain mesh from a point cloud.
 
 import dtcc
 
-# Define the bounding box for the Helsingborg residential area. 
-# These coordinates specify the area of interest to be passed into the 
-bounds = dtcc.Bounds(
-    xmin=319891,
-    ymin=6399790,
-    xmax=319891+2000,
-    ymax=6399790+2000
-)
+# Define bounds (a residential area in Helsingborg)
+h = 2000.0
+bounds = dtcc.Bounds(319891, 6399790, 319891 + h, 6399790 + h)
 
-pc = dtcc.download_data(data_type='lidar', provider='dtcc', user_bbox=bounds)
-pointcloud = pc.remove_global_outliers(3.0)
+# Dowload point cloud and footprints
+pointcloud = dtcc.download_pointcloud(bounds=bounds)
+footprints = dtcc.download_footprints(provider="OSM", bounds=bounds)
 
-# load footprints
-footprints = dtcc.download_data(data_type='footprints', provider='OSM', user_bbox=bounds)
+# Remove global outliers
+pointcloud = pointcloud.remove_global_outliers(3.0)
 
-# merge and clean up footprints
-merged_footprints = dtcc.merge_building_footprints(
+# FIXME: Rename merge_building_footprints --> merge_footprints etc
+
+# Merge and clean footprints
+footprints = dtcc.merge_building_footprints(
     footprints, lod=dtcc.GeometryType.LOD0, max_distance=0.5, min_area=10
 )
 
-simplifed_footprints = dtcc.simplify_building_footprints(
-    merged_footprints, 0.25, lod=dtcc.GeometryType.LOD0
+footprints = dtcc.simplify_building_footprints(
+    footprints, 0.25, lod=dtcc.GeometryType.LOD0
 )
 
-clearance_fix = dtcc.fix_building_footprint_clearance(simplifed_footprints, 0.5)
+footprints = dtcc.fix_building_footprint_clearance(footprints, 0.5)
 
+# Extract footprint surfaces
+footprints = [building.lod0 for building in footprints]
 
-# extract footprint surfaces
-footprints = [building.lod0 for building in clearance_fix]
-
-# set subdomain resolution every other building to 2 and the rest to 0.5
+# Set subdomain resolution every other building to 2 and the rest to 0.5
 subdomain_resolution = [0.5] * len(footprints)
 for i in range(len(footprints)):
     if i % 2 == 0:
         subdomain_resolution[i] = 2
 
-
-terrain = dtcc.build_terrain_mesh(
+# Build terrain mesh
+mesh = dtcc.build_terrain_mesh(
     pointcloud,
     subdomains=footprints,
     subdomain_resolution=subdomain_resolution,
@@ -51,4 +45,5 @@ terrain = dtcc.build_terrain_mesh(
     smoothing=3,
 )
 
-terrain.mesh.view()
+# View terrain mesh
+mesh.view()

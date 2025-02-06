a99454f8fb991d6bce07db01d6f138ca565f6d7b
Anders Logg <logg@chalmers.se>
2025-02-06 14:13:53 +0100

    Demo polishing

--- demos/view_pointcloud.py
+++ demos/view_pointcloud.py
@@ -1,18 +1,14 @@
-# This demo illustrates how to load and view a point cloud.
+# This demo illustrates how to download and view a point cloud.
 
 import dtcc
 
-# Define bounds (in this case a residential area in Helsingborg)
-bounds = dtcc.Bounds(
-    xmin=319891,
-    ymin=6399790,
-    xmax=319891+2000,
-    ymax=6399790+2000
-)
+# Define bounds (a residential area in Helsingborg)
+h = 2000.0
+bounds = dtcc.Bounds(319891, 6399790, 319891 + h, 6399790 + h)
 
 # Download point cloud
-pc = dtcc.download_data(data_type="lidar", provider="dtcc", user_bbox=bounds)
+pointcloud = dtcc.download_pointcloud(bounds=bounds)
 
 # View point cloud (color by z-coordinate)
-color_data = pc.points[:, 2]
-pc.view(data=color_data)
+color_data = pointcloud.points[:, 2]
+pointcloud.view(data=color_data)

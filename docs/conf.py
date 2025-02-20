c6a5ef7bfa812e3d6f15765539cfc478f9dc347f
Anders Logg <logg@chalmers.se>
2025-02-20 12:18:03 +0100

    Add version number to docs, extracted from pyproject.toml

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,15 @@
-import os
+import os, toml
+
+# Parse version from pyproject.toml
+with open("../pyproject.toml", "r") as f:
+    data = toml.load(f)
+version = data["project"]["version"]
 
 # General information about the project
 project = "DTCC Platform"
 author = "Digital Twin Cities Centre"
 copyright = "Digital Twin Cities Centre (2025)"
-version = "develop"
+release = version
 
 # Determine whether to skip API docs generation.
 skip_api = os.environ.get("NO_API", "0") == "1"
@@ -17,6 +22,7 @@ extensions = [
     "sphinx_immaterial",
 ]
 
+# Skip or include API
 if skip_api:
     print("Skipping building API docs")
     exclude_patterns = ["_api"]
@@ -34,12 +40,15 @@ autosectionlabel_prefix_document = True
 
 # HTML output
 html_theme = "sphinx_immaterial"
+html_title = f"DTCC Platform (v{version})"
 html_theme_options = {
     "version_dropdown": True,
     "version_info": [
-        {"version": "2.0", "title": "2.0", "aliases": ["latest"]},
-        {"version": "1.0", "title": "1.0", "aliases": []},
+        {"version": f"v{version}", "title": f"v{version}", "aliases": []},
+        {"version": "v0.7.0", "title": "v0.7.0", "aliases": []},
+        {"version": "v0.6.0", "title": "v0.6.0", "aliases": []},
     ],
 }
 html_static_path = ["_static"]
 html_css_files = ["custom.css"]
+html_logo = "_static/dtcc-icon.svg"

ad66e57223b1930eeec37e9caa73bb77d112e659
Dag Wästberg <dwastberg@gmail.com>
2025-02-21 15:32:15 +0100

    new city model

--- demos/build_city_mesh_from_cityjson.py
+++ demos/build_city_mesh_from_cityjson.py
@@ -5,8 +5,13 @@ from urllib.request import urlretrieve
 import tempfile
 import os
 
+from dtcc_core.io.footprints import building_bounds
+
 # Download example CityJSON file
-url = "https://3d.bk.tudelft.nl/opendata/cityjson/3dcities/v2.0/DenHaag_01.city.json"
+# url = "https://3d.bk.tudelft.nl/opendata/cityjson/3dcities/v2.0/DenHaag_01.city.json"
+url = (
+    "https://3d.bk.tudelft.nl/opendata/cityjson/3dcities/v2.0/3-20-DELFSHAVEN.city.json"
+)
 temp_path = tempfile.NamedTemporaryFile(suffix=".json", delete=False)
 urlretrieve(url=url, filename=temp_path.name)
 
@@ -14,6 +19,8 @@ urlretrieve(url=url, filename=temp_path.name)
 city = dtcc.load_city(temp_path.name)
 
 
+if not city.has_terrain():
+    city.add_flat_terrain(buffer=10)
 # Build city mesh
 mesh = dtcc.build_city_mesh(city, dtcc.GeometryType.LOD2)
 

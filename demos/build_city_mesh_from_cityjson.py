d8558e09d177011a81c6387b3fa3513cd2042815
Dag Wästberg <dwastberg@gmail.com>
2025-02-21 14:05:19 +0100

    clean up download

--- demos/build_city_mesh_from_cityjson.py
+++ demos/build_city_mesh_from_cityjson.py
@@ -2,16 +2,21 @@
 
 import dtcc
 from urllib.request import urlretrieve
+import tempfile
+import os
 
 # Download example CityJSON file
 url = "https://3d.bk.tudelft.nl/opendata/cityjson/3dcities/v2.0/DenHaag_01.city.json"
-urlretrieve(url=url, filename="city.json")
+temp_path = tempfile.NamedTemporaryFile(suffix=".json", delete=False)
+urlretrieve(url=url, filename=temp_path.name)
 
 # Load city model from CityJSON file
-city = dtcc.load_city("city.json")
+city = dtcc.load_city(temp_path.name)
+
 
 # Build city mesh
 mesh = dtcc.build_city_mesh(city, dtcc.GeometryType.LOD2)
 
 # View mesh
 mesh.view()
+os.unlink(temp_path.name)

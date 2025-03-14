6901858c59e973eb469606bbec069f39b448ab42
Themis <themistoklisarv2001@gmail.com>
2025-03-14 10:30:27 +0100

    Purged unused files as per issue #84

--- workflows/workflow_sanjay.py
+++
@@ -1,5 +0,0 @@
-# Copyright (C) 2023 Firstname Lastname
-# Licensed under the MIT License
-#
-# This script documents the workflow for Sanjay.
-# https://github.com/dtcc-platform/dtcc/issues/54
--- workflows/workflow_table.py
+++
@@ -1,5 +0,0 @@
-# Copyright (C) 2023 Firstname Lastname
-# Licensed under the MIT License
-#
-# This script documents the workflow for the 3D table.
-# https://github.com/dtcc-platform/dtcc/issues/57

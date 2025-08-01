5df32f29c08597b506588fe78b82b97569882780
georspai <spaikgeo@gmail.com>
2025-08-01 14:41:37 +0300

    Added 'build_city_volume_mesh' demo

---
+++ demos/build_city_volume_mesh.py
@@ -0,0 +1,38 @@
+# This demo illustrates how to build a city mesh from a point cloud and footprints.
+
+import dtcc
+
+# Define bounds (a residential area in Helsingborg)
+h = 200.0
+bounds = dtcc.Bounds(319891, 6399790, 319891 + h, 6399790 + h)
+
+# Download pointcloud and building footprints
+pointcloud = dtcc.download_pointcloud(bounds=bounds)
+buildings = dtcc.download_footprints(bounds=bounds)
+
+# Remove global outliers
+pointcloud = pointcloud.remove_global_outliers(3.0)
+
+# Build terrain raster
+raster = dtcc.build_terrain_raster(pointcloud, cell_size=2, radius=3, ground_only=True)
+
+# Extract roof points and compute building heights
+buildings = dtcc.extract_roof_points(buildings, pointcloud)
+buildings = dtcc.compute_building_heights(buildings, raster, overwrite=True)
+
+# Create city and add geometries
+city = dtcc.City()
+city.add_terrain(raster)
+city.add_buildings(buildings, remove_outside_terrain=True)
+
+# Build city volume mesh
+domain_height = 50.0  # Height of the volume mesh domain
+max_mesh_size = 10.0  # Maximum size of the mesh triangles
+
+volume_mesh = dtcc.build_city_volume_mesh(city, 
+                                          domain_height=domain_height, 
+                                          max_mesh_size=max_mesh_size,
+                                          boundary_face_markers=False)
+
+# View mesh
+volume_mesh.view()

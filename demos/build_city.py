956016db68f59c235b61040386a875139194a631
Dag Wästberg <dwastberg@gmail.com>
2025-09-15 13:35:54 +0200

    update build city demo

--- demos/build_city.py
+++ demos/build_city.py
@@ -4,31 +4,20 @@ import dtcc
 h = 2000.0
 bounds = dtcc.Bounds(319891, 6399790, 319891 + h, 6399790 + h)
 
-# Download pointcloud and building footprints
-pointcloud = dtcc.download_pointcloud(bounds=bounds)
-buildings = dtcc.download_footprints(bounds=bounds)
+city = dtcc.City()
+city.bounds = bounds
 
-# Remove global outliers
-pointcloud = pointcloud.remove_global_outliers(3.0)
+city.download_footprints()
+city.download_pointcloud()
 
-# Build terrain raster and mesh
-raster = dtcc.builder.build_terrain_raster(pointcloud, cell_size=5, ground_only=True)
-mesh = dtcc.builder.build_terrain_mesh(raster)
-
-# Extract roof points and compute building heights
-buildings = dtcc.extract_roof_points(buildings, pointcloud)
-buildings = dtcc.compute_building_heights(buildings, raster, overwrite=True)
-
-# Build LOD1 buildings
-buildings = dtcc.builder.build_lod1_buildings(
-    buildings, default_ground_height=raster.min, always_use_default_ground=True
+city.build_terrain(
+    cell_size=2.0,
+    build_mesh=True,
+    max_triangle_size=5.0,
+    smoothing=3,
 )
 
-# Create city and add buildings and geometries
-city = dtcc.City()
-city.add_buildings(buildings)
-city.add_terrain(raster)
-city.add_terrain(mesh)
+city.build_lod1_buildings()
 
 # View city
 city.view()

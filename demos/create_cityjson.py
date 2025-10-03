eb86c2eeddd98056f5b87aa868ed8ea9c94aeea7
Dag Wästberg <dwastberg@gmail.com>
2025-10-03 11:40:54 +0200

    write cityjson demo

---
+++ demos/create_cityjson.py
@@ -0,0 +1,21 @@
+from dtcc import City, Bounds
+
+# Define bounds (a residential area in Helsingborg)
+h = 2000.0
+bounds = Bounds(319891, 6399790, 319891 + h, 6399790 + h)
+
+city = City()
+city.bounds = bounds
+
+city.download_footprints()
+city.download_pointcloud()
+
+city.build_terrain(
+    cell_size=2.0,
+    build_mesh=True,
+    max_triangle_size=5.0,
+    smoothing=3,
+)
+
+
+city.save("test_city.json")

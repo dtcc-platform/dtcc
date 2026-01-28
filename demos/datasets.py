4f814080ca181fc23bd09c45be8a8c7dcd755dc1
Anders Logg <logg@chalmers.se>
2026-01-28 17:37:45 +0100

    Improve datasets demo

--- demos/datasets.py
+++ demos/datasets.py
@@ -6,4 +6,20 @@ from dtcc import datasets, Bounds
 h = 2000.0
 bounds = Bounds(319891, 6399790, 319891 + h, 6399790 + h)
 
-datasets.pointcloud(bounds=bounds)
+# Print information about all available datasets
+datasets.info()
+
+# Get point cloud dataset
+pointcloud = datasets.pointcloud(bounds=bounds)
+print(pointcloud)
+pointcloud.view()
+
+# Get terrain dataset
+terrain = datasets.terrain(bounds=bounds)
+print(terrain)
+terrain.view()
+
+# Get buildings dataset (and view the first building)
+buildings = datasets.buildings(bounds=bounds)
+print(buildings[0])
+buildings[0].view()
